7bfff94fc5abf2884fe6e41085259200aaa4fb3c
2933738083 <2933738083@qq.com>
2023-05-13 16:40:38 +0800

    Site updated: 2023-05-13 16:40:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/10/node天下无敌-1/","2023/05/10/hello-world/","2023/05/10/前端三剑客-1/","2023/05/10/vuex学习/","2023/05/10/日常的生活-1/","2023/05/10/编程学习-1/","2023/05/10/什么叫C/","2023/05/10/胡思乱想-1/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
+var posts=["2023/05/10/hello-world/","2023/05/10/前端三剑客-1/","2023/05/10/node天下无敌-1/","2023/05/12/vuex学习/","2023/05/10/编程学习-1/","2023/05/10/日常的生活-1/","2023/05/10/什么叫C/","2023/05/10/胡思乱想-1/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
